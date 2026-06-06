6cef31d9515e97a5af3bf4592cec383c575057ef
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-06 17:38:50 -0300

    feito e revisado

--- Lista/ListaB1.c
+++ Lista/ListaB1.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+struct aluno{
+    char nome[50];
+    float nota;
+};
+int main(){
+    int N, i, aprovados=0;
+    float soma=0, media;
+    printf("Digite o numero de alunos: ");
+    scanf("%d", &N);
+    struct aluno turma[N];
+
+    for(i=0; i<N; i++){
+        printf("Digite o nome do aluno: ");
+        scanf("%s", turma[i].nome);
+        printf("Digite a nota do aluno: ");
+        scanf("%f", &turma[i].nota);
+
+        soma += turma[i].nota;
+        if(turma[i].nota >= 7.0){
+            aprovados++;
+        }
+    }
+    media = soma / N;
+
+    printf("\n------tabela da turma------\n");
+    printf("Nome       Nota\n");
+    for(i=0; i<N; i++){
+        printf("%-10s %.2f\n", turma[i].nome, turma[i].nota);
+    }
+    printf("\nMedia da turma: %.2f\n", media);
+    printf("Alunos aprovados: %d\n", aprovados);
+    return 0;
+}
+
 
