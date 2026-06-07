8d69d3214957f1c563e773ef750c0a852416f7f0
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-07 00:02:24 -0300

    feito

--- Lista/ListaC4.c
+++ Lista/ListaC4.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
+struct aluno {
+    char nome[50];
+    float nota1 ,nota2, nota3;
+    float media;
+};
 
+struct aluno calcularmedia(struct aluno a) {
+    a.media = (a.nota1 + a.nota2 + a.nota3) / 3;
+    return a;
+}
+
+char definirconceito(float media) {
+    if (media >= 9) {
+        return 'A';
+    } else if (media >= 7) {
+        return 'B';
+    } else if (media >= 5) {
+        return 'C';
+    } else    
+        return 'D';
+    
+    }
+
+    void imprimiraluno(struct aluno a) {
+        printf("%s - media: %.2f - conceito: %c\n", a.nome, a.media, definirconceito(a.media));
+    }
+    void ordenarPorNota(struct aluno turma[], int n) {
+        int i, j, maior;
+        struct aluno aux;
+        for (i = 0; i < n - 1; i++) {
+            maior = i;
+            for (j = i + 1; j < n; j++) {
+                if (turma[j].media > turma[maior].media) {
+                    maior = j;
+                }
+            }
+            if (maior != i) {
+                aux = turma[i];
+                turma[i] = turma[maior];
+                turma[maior] = aux;
+            }
+        }
+    }
+int main() {
+    int n, i;
+    printf("Digite o numero de alunos: ");
+    scanf("%d", &n);
+    struct aluno alunos[n];
+    for (i = 0; i < n; i++) {
+        printf("Digite o nome do aluno: ");
+        scanf("%s", alunos[i].nome);
+        printf("Digite as tres notas do aluno: ");
+        scanf("%f %f %f", &alunos[i].nota1, &alunos[i].nota2, &alunos[i].nota3);
+        alunos[i] = calcularmedia(alunos[i]);
+    }
+    ordenarPorNota(alunos, n);
+    printf("\n------ boletim ------\n");
+    for (i = 0; i < n; i++) {
+        imprimiraluno(alunos[i]);
+    }
+    return 0;
+}
