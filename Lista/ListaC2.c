a78f79ae419b27061cd4cf1f141a3f62785fc587
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-06 23:24:44 -0300

    feito

--- Lista/ListaC2.c
+++ Lista/ListaC2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+void fizzbuzz(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf(" FizzBuzz ");
+        } else if (i % 3 == 0) {
+            printf(" Fizz ");
+        } else if (i % 5 == 0) {
+            printf(" Buzz ");
+        } else {
+            printf("%d", i);
+        }
+    }
+}
+int main() {
+    int n;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n);
+    fizzbuzz(n);
+    return 0;
+}
