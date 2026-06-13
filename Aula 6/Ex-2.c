571e9702c53d5786302dfb308759fc4b8d478cde
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-12 23:04:13 -0300

    Update Ex-2.c

--- Aula 6/Ex-2.c
+++ Aula 6/Ex-2.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 
-struct produto {
+struct aluno {
     char nome[50];
-    float preco;
-    int quantidade;
+    float nota;
 };
+
 int main() {
-    struct produto p;
+    int n, i, acimadamedia = 0;
+    float soma = 0, media;
 
-    printf("Digite o nome do produto: ");
-    scanf("%s", p.nome);
+    printf("Digite o numero de alunos: ");
+    scanf("%d", &n);
 
-    printf("Digite o preço do produto: ");
-    scanf("%f", &p.preco);
+    struct aluno turma[n];
 
-    printf("Digite a quantidade do produto: ");
-    scanf("%d", &p.quantidade);
+    for (i = 0; i < n; i++) {
+        printf("Digite o nome do aluno %d: ", i + 1);
+        scanf("%s", turma[i].nome);
+        printf("Digite a nota do aluno %d: ", i + 1);
+        scanf("%f", &turma[i].nota);
+        soma += turma[i].nota;
+    }
 
-    printf("\nnome: %s\n", p.nome);
-    printf("Preço: %.2f\n", p.preco);
-    printf("Quantidade: %d\n", p.quantidade);
-    printf("Valor total em estoque: %.2f\n", p.preco * p.quantidade);
+    media = soma / n;
+
+    printf("\n------Boletim------\n");
+    printf("Nome\tNota\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%s\t%.2f\n", turma[i].nome, turma[i].nota);//
+        if (turma[i].nota > media) {
+            acimadamedia++;
+        }
+    }
+
+    printf("Media da turma: %.2f\n", media);
+    printf("Alunos acima da media: %d\n", acimadamedia);
 
     return 0;
 }
