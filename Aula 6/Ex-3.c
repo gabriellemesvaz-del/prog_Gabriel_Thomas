35970e6513d6efc75d08f368a0cfa071c7e4e13a
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-12 23:25:57 -0300

    Update Ex-3.c

--- Aula 6/Ex-3.c
+++ Aula 6/Ex-3.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+struct aluno {
+	char nome[50];
+	float nota;
+};
+
+int main() {
+	int n, i;
+
+	printf("Digite o numero de alunos: ");
+	scanf("%d", &n);
+
+	struct aluno turma[n];
+
+	for (i = 0; i < n; i++) {
+		printf("Digite o nome do aluno %d: ", i + 1);
+		scanf("%s", turma[i].nome);
+		printf("Digite a nota do aluno %d: ", i + 1);
+		scanf("%f", &turma[i].nota);
+}
+		int maior = 0, menor = 0;
+
+		for (i = 1; i < n; i++) {
+			if (turma[i].nota > turma[maior].nota) {
+				maior = i;
+			}
+		}
+
+		printf("\naluno com maior nota:\n");
+		printf("nome: %s\n", turma[maior].nome);
+		printf("nota: %.2f\n", turma[maior].nota);
+
+		printf("\naluno com menor nota:\n");
+		for (i = 1; i < n; i++) {
+			if (turma[i].nota < turma[menor].nota) {
+				menor = i;
+			}
+		}
+		printf("nome: %s\n", turma[menor].nome);
+		printf("nota: %.2f\n", turma[menor].nota);
+
+		return 0;
+	}
