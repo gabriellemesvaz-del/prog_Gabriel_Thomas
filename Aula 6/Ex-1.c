5474abc0f1868fb023eebb0cba2b9d86df5e28c5
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-12 22:46:19 -0300

    feito

--- Aula 6/Ex-1.c
+++ Aula 6/Ex-1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+struct produto {
+    char nome[50];
+    float preco;
+    int quantidade;
+};
+int main() {
+    struct produto p;
+
+    printf("Digite o nome do produto: ");
+    scanf("%s", p.nome);
+
+    printf("Digite o preço do produto: ");
+    scanf("%f", &p.preco);
+
+    printf("Digite a quantidade do produto: ");
+    scanf("%d", &p.quantidade);
+
+    printf("\nnome: %s\n", p.nome);
+    printf("Preço: %.2f\n", p.preco);
+    printf("Quantidade: %d\n", p.quantidade);
+    printf("Valor total em estoque: %.2f\n", p.preco * p.quantidade);
+
+    return 0;
+}
