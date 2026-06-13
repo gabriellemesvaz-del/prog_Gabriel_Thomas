535fcf87b001937e482f3fa82b001253aa808d50
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-13 15:10:46 -0300

    Feito

--- Aula 7/ex-1.c
+++ Aula 7/ex-1.c
@@ -1,1 +1,36 @@
 
+#include <stdio.h>
+
+float somar(float a, float b) {
+    return a + b;
+}
+
+float subtrair(float a, float b) {
+    return a - b;
+}
+
+float multiplicar(float a, float b) {
+    return a * b;
+}
+
+float dividir(float a, float b) {
+    if (b == 0) {
+        printf("Erro: divisao por zero!\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int main() {
+    float n1, n2;
+
+    printf("Digite dois numeros: ");
+    scanf("%f %f", &n1, &n2);
+
+    printf("Soma: %.2f\n", somar(n1, n2));
+    printf("Subtracao: %.2f\n", subtrair(n1, n2));
+    printf("Multiplicacao: %.2f\n", multiplicar(n1, n2));
+    printf("Divisao: %.2f\n", dividir(n1, n2));
+
+    return 0;
+}
