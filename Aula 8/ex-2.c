8fa08dcd4b8d5ec8d608559cc022bbadc5f3dfe2
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-13 23:32:23 -0300

    Feito

--- Aula 8/ex-2.c
+++ Aula 8/ex-2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int potencia(int base, int exp);
+
+int main() {
+    int base, exp;
+
+    printf("Digite a base: ");
+    scanf("%d", &base);
+
+    printf("Digite o expoente: ");
+    scanf("%d", &exp);
+
+    printf("Resultado = %d\n", potencia(base, exp));
+
+    return 0;
+}
+
+int potencia(int base, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+
+    return base * potencia(base, exp - 1);
+}
