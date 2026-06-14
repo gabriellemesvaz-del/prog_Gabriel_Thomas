f771f88fe422142388bc31bea8055f0240c17675
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-13 23:33:37 -0300

    Feito

--- Aula 8/ex-3.c
+++ Aula 8/ex-3.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+void regressiva(int n);
+void progressiva(int n);
+
+int main() {
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("\nContagem regressiva:\n");
+    regressiva(n);
+
+    printf("\n\nContagem progressiva:\n");
+    progressiva(n);
+
+    return 0;
+}
+
+void regressiva(int n) {
+    if (n == 0) {
+        printf("Vai!\n");
+        return;
+    }
+
+    printf("%d\n", n);
+    regressiva(n - 1);
+}
+
+void progressiva(int n) {
+    if (n == 0) {
+        return;
+    }
+
+    progressiva(n - 1);
+    printf("%d\n", n);
+}
