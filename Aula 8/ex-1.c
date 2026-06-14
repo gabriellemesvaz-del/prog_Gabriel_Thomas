98f04a1c8b37efdc1c4df95728705f6cea972f99
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-13 23:30:56 -0300

    Feito

--- Aula 8/ex-1.c
+++ Aula 8/ex-1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int soma(int n);
+
+int main() {
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("Soma = %d\n", soma(n));
+
+    return 0;
+}
+
+int soma(int n) {
+    if (n == 0) {
+        return 0; 
+    }
+
+    return n + soma(n - 1);
+}
