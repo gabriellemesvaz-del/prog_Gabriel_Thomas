ea03da46c8f8886b0331b48473600bf8679ebabc
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-06-13 23:34:33 -0300

    Feito

--- Aula 8/ex-4.c
+++ Aula 8/ex-4.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int fibonacci(int n);
+
+int main() {
+    int i;
+
+    printf("Primeiros 10 termos da sequencia de Fibonacci:\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0; // caso-base
+    }
+
+    if (n == 1) {
+        return 1; // caso-base
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
