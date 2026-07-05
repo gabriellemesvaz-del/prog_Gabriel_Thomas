06162ba9b4b3f549b21f7d675ca0f729fe46bbdb
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-07-04 23:36:30 -0300

    Implement recursive function to find maximum element

--- Aula 9/Ex-1.c
+++ Aula 9/Ex-1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int maior(int v[], int n) {
+  
+    if (n == 1) {
+        return v[0];
+    }
+
+    int maiorResto = maior(v, n - 1);
+
+    if (v[n - 1] > maiorResto) {
+        return v[n - 1];
+    } else {
+        return maiorResto;
+    }
+}
+
+int main() {
+    int v[] = {12, 7, 25, 4, 18, 30, 9};
+    int n = 7; 
+
+    printf("Maior elemento: %d\n", maior(v, n));
+
+    return 0;
+}
