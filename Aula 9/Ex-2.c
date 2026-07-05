5b797455c61cce9d595dd7481acd8656fc2bf2bf
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-07-04 23:39:31 -0300

    Implement string inversion function

--- Aula 9/Ex-2.c
+++ Aula 9/Ex-2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+void inverter(char s[], int ini, int fim) {
+
+  if (ini >= fim) {
+        return;
+    }
+
+
+    char temp = s[ini];
+    s[ini] = s[fim];
+    s[fim] = temp;
+
+    inverter(s, ini + 1, fim - 1);
+}
+
+int main() {
+    char s[] = "CEEP";
+
+    inverter(s, 0, 3);
+
+    printf("String invertida: %s\n", s);
+
+    return 0;
+}
