7acbc96c5749c319f980613642887e4c3d3ec09f
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-08-03 22:12:38 -0300

    Add function to calculate sum of digits

--- lista2/Q3-A.c
+++ lista2/Q3-A.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int somaDigitos(int n) {
+    if (n == 0)
+        return 0;
+
+    return (n % 10) + somaDigitos(n / 10);
+}
+
+int main() {
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("Soma dos digitos = %d\n", somaDigitos(n));
+
+    return 0;
+}
