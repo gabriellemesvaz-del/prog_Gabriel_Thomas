05eb803af0351041232b1c8936e754deb30cf83c
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-08-04 11:28:41 -0300

    Update Q3-C.c

--- lista2/Q3-C.c
+++ lista2/Q3-C.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int Palindromo(char s[], int ini, int fim)
+{
+    if (ini >= fim)
+        return 1;
+
+    if (s[ini] != s[fim])
+        return 0;
+
+    return Palindromo(s, ini + 1, fim - 1);
+}
+
+int main()
+{
+    char palavra[100];
+    int i = 0;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+
+    // Calcula o tamanho da string sem usar strlen
+    while (palavra[i] != '\0')
+    {
+        i++;
+    }
+
+    if (Palindromo(palavra, 0, i - 1))
+        printf("é palindromo.\n");
+    else
+        printf("Nao é palindromo.\n");
+
+    return 0;
+}
