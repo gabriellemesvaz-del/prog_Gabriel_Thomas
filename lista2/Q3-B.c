6aad28e71a79488bed0a56a6bec6e39822c21a57
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-08-03 22:14:01 -0300

    Add function to count occurrences of a character

--- lista2/Q3-B.c
+++ lista2/Q3-B.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int contarChar(char s[], int i, char c) {
+    if (s[i] == '\0')
+        return 0;
+
+    if (s[i] == c)
+        return 1 + contarChar(s, i + 1, c);
+
+    return contarChar(s, i + 1, c);
+}
+
+int main() {
+    char s[100];
+    char c;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", s);
+
+    printf("Digite o caractere: ");
+    scanf(" %c", &c);
+
+    printf("Quantidade de '%c': %d\n", c, contarChar(s, 0, c));
+
+    return 0;
+}
