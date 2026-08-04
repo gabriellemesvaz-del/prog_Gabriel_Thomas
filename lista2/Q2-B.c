d6f972642bcce74705a050e8b77914424aa4052f
gabriellemesvaz-del <gabriel.lemes.vaz@escola.pr.gov.br>
2026-08-03 21:59:14 -0300

    Update Q2-B.c

--- lista2/Q2-B.c
+++ lista2/Q2-B.c
@@ -1,1 +1,23 @@
+Descida:
+fatorial(4)
+↓
+4 × fatorial(3)
+↓
+3 × fatorial(2)
+↓
+2 × fatorial(1)
+↓
+1 × fatorial(0)
+↓
+1 (caso-base)
 
+Subida:
+fatorial(0) = 1
+↑
+fatorial(1) = 1 × 1 = 1
+↑
+fatorial(2) = 2 × 1 = 2
+↑
+fatorial(3) = 3 × 2 = 6
+↑
+fatorial(4) = 4 × 6 = 24
